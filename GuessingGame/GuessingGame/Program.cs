2457d5fc572a4d244ee6efcbb44efe722ac20dc4
GT1876 <garrysontime@gmail.com>
2014-03-05 10:05:55 -0500

    sdiugadh

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -9,6 +9,21 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            int guess;
+            int guess2;
+            String Input;
+            while (true)
+            {
+                guess = 50;
+                guess2 = 75;
+                Console.WriteLine("Is your guess {0}? h,l,e", guess, guess2);
+                Input = Console.ReadLine();
+                if (Input == "h");
+                {
+
+
+                }
+            }
         }
     }
 }
